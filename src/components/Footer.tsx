d26771705572c33f315ac1eb2a0fdbf623083067
seo2021 <76505625+seo2021@users.noreply.github.com>
2025-03-08 15:09:30 +0900

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,13 +9,6 @@ const Footer = ({
 }) => {
   return (
     <footer className="flex flex-col gap-2 justify-center items-center bg-GRAY h-28 text-xs text-white dark:bg-GRAY_EXTRAHEAVY dark:text-GRAY_HEAVY">
-      <div className="flex gap-1" style="display: none">
-        {contact.map((contact) => (
-          <ContactItem key={contact.id} {...contact}>
-            {contact.name}
-          </ContactItem>
-        ))}
-      </div>
       <div className="flex flex-col items-center">
         <span className="whitespace-pre-wrap text-center">{`Copyright 2025. 서은정 All rights reserved.`}</span>
         <a target="_blank" rel="noreferrer" href="https://github.com/sjoleee/very-simple-portfolio">

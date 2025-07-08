55487c10291b14105a43596898a0bf799ee2e928
seo2021 <76505625+seo2021@users.noreply.github.com>
2025-07-08 21:39:32 +0900

    Update index.tsx

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -10,7 +10,7 @@ const Information = ({ information }: Pick<DataProps, "information">) => {
       <div className="flex flex-col gap-2">
         <h1 className="leading-[1.15]">
           {/* 포지션에 맞게 문구를 수정해주세요. 혹은, 본인이 원하는 대로 문구를 바꿔주세요. */}
-          안녕하세요, 3년차
+          안녕하세요,
           <br /> Java 기반 웹 서비스 개발과 시스템 운영을 함께 해온{" "}
           <span className="text-PRIMARY font-semibold">{information.name}</span>
           입니다.

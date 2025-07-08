d5b1c44bae7bef2a9ec42938bf22d7b15576006d
seo2021 <76505625+seo2021@users.noreply.github.com>
2025-07-08 21:45:28 +0900

    Update index.tsx

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -8,13 +8,13 @@ const Information = ({ information }: Pick<DataProps, "information">) => {
   return (
     <div className="flex flex-col gap-8">
       <div className="flex flex-col gap-2">
-        <h1 className="leading-[1.15]">
+        <h2 className="leading-[1.15]">
           {/* 포지션에 맞게 문구를 수정해주세요. 혹은, 본인이 원하는 대로 문구를 바꿔주세요. */}
           안녕하세요,
           <br /> 웹 개발 3년, 운영 1년 경력의{" "}
           <span className="text-PRIMARY font-semibold">{information.name}</span>
           입니다.
-        </h1>
+        </h2>
         <div className="flex gap-1">
           {information.contact.map((contact) => (
             <ContactItem
